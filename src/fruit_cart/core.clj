09d14c67bacd8e950c734a98aec18c9791a30bb3
Blaine Price <1wbprice@gmail.com>
2016-12-11 23:16:42 -0500

    Print a list of fruits.

--- src/fruit_cart/core.clj
+++ src/fruit_cart/core.clj
@@ -1,7 +1,9 @@
 (ns fruit-cart.core
+(require fruit-cart.fruit.fruit-list)
+(refer fruit-cart.fruit.fruit-list)
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (fruits)))
