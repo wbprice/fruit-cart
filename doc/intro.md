094cf5c5c277cc71606b605897361cd7cb705627
Blaine Price <1wbprice@gmail.com>
2016-12-11 22:24:34 -0500

    Initial commit.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to produce-stand
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/produce_stand/core.clj
@@ -0,0 +1,7 @@
+(ns produce-stand.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
